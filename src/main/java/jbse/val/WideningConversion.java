2155e292f3ee65ffcf634ab047cc378dddfc06aa
danglotb <bdanglot@gmail.com>
2018-03-02 09:39:26 +0100

    refactor: add supertype conversion for narrow and widen conversion

--- src/main/java/jbse/val/WideningConversion.java
+++ src/main/java/jbse/val/WideningConversion.java
@@ -5,40 +5,27 @@ import jbse.val.exc.InvalidOperandException;
 import jbse.val.exc.InvalidTypeException;
 import jbse.val.exc.ValueDoesNotSupportNativeException;
 
-public final class WideningConversion extends Primitive {
-	private final Primitive arg;
-	private final String toString;
-	private final int hashCode;
+public final class WideningConversion extends Conversion {
 
-	private WideningConversion(char type, Calculator calc, Primitive arg) 
-	throws InvalidOperandException, InvalidTypeException {
-		super(type, calc);
-
-		//checks on parameters
-        if (arg == null) {
-    		throw new InvalidOperandException("null operand in widening construction");
-        }
-		if (!Type.widens(type, arg.getType())) {
-			throw new InvalidTypeException("cannot widen type " + arg.getType() + " to type " + type);
-		}
-		this.arg = arg;
-
-		//calculates hashCode
-		final int prime = 281;
-		int result = 1;
-		result = prime * result + arg.hashCode();
-		result = prime * result + type;
-		this.hashCode = result;
-		
-		//calculates toString
-		this.toString = "WIDEN-"+ this.getType() + "(" + arg.toString() + ")";
+	protected WideningConversion(char type, Calculator calc, Primitive arg) throws InvalidOperandException, InvalidTypeException {
+		super(type, calc, arg);
 	}
-	
-	public static WideningConversion make(char type, Calculator calc, Primitive arg) 
+
+	public static WideningConversion make(char type, Calculator calc, Primitive arg)
 	throws InvalidOperandException, InvalidTypeException {
 		return new WideningConversion(type, calc, arg);
 	}
-	
+
+	@Override
+	protected String conversionAsString() {
+		return "widen";
+	}
+
+	@Override
+	protected int getPrime() {
+		return 281;
+	}
+
 	public Primitive getArg() {
 		return this.arg;
 	}
@@ -48,55 +35,4 @@ public final class WideningConversion extends Primitive {
 		v.visitWideningConversion(this);
 	}
 
-	@Override
-	public boolean surelyTrue() {
-		return false;
-	}
-
-	@Override
-	public boolean surelyFalse() {
-		return false;
-	}
-
-	@Override
-	public boolean isSymbolic() {
-		return true;
-	}
-
-	@Override
-	public Object getValueForNative() throws ValueDoesNotSupportNativeException {
-		throw new ValueDoesNotSupportNativeException();
-	}
-
-	@Override
-	public String toString() {
-		return this.toString;
-	}
-
-	@Override
-	public int hashCode() {
-		return this.hashCode;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final WideningConversion other = (WideningConversion) obj;
-		if (arg == null) {
-			if (other.arg != null) {
-				return false;
-			}
-		} else if (!arg.equals(other.arg)) {
-			return false;
-		}
-		return true;
-	}
 }
